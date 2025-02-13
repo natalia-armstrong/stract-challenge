799b0e0756d7f89b3a11067d792418a251bd185e
Natalia Armstrong <156014605+natalia-armstrong@users.noreply.github.com>
2025-02-13 19:23:21 -0300

    Docker: Adicionado dockerfile para execução via docker

--- README.md
+++ README.md
@@ -1,1 +1,153 @@
-# scract-challenge
+# **Stract Challenge** 
+
+Este repositório contém uma API desenvolvida para consumir dados de plataformas de anúncios e gerar relatórios em formato CSV. A aplicação foi construída utilizando **Flask** e **Docker**. O desafio propõe a criação de endpoints que recuperam insights de anúncios, fornecendo tabelas dinâmicas e agregadas com base nas informações obtidas.
+
+## **Passos para Rodar a Aplicação**
+
+### 1. **Clonar o Repositório**
+
+Clone o projeto utilizando o comando:
+
+```bash
+git clone https://github.com/natalia-armstrong/scract-challenge.git
+```
+
+### 2. Criar e Ativar o Ambiente Virtual
+#### 2.1 Criar o Ambiente Virtual
+No terminal, na raiz do projeto, execute o comando para criar um ambiente virtual:
+
+Windows:
+```bash
+python -m venv venv
+```
+Linux:
+```bash
+python3 -m venv venv
+```
+#### 2.2 Ativar o Ambiente Virtual
+Execute o seguinte comando no terminal:
+
+Windows:
+```bash
+venv\Scripts\activate
+```
+Linux:
+```bash
+source venv/bin/activate
+```
+Após a ativação, o nome do ambiente virtual aparecerá no terminal.
+
+### 3. Instalar Dependências
+Com o ambiente virtual ativo, instale as dependências do projeto com o comando:
+
+```bash
+pip install -r requirements.txt
+```
+
+### 4. Rodar a Aplicação
+Agora que as dependências estão instaladas, você pode rodar a aplicação de duas formas: utilizando Docker ou diretamente no terminal.
+
+#### 4.1 Rodar a Aplicação com Docker
+
+Abra o [Docker Desktop](https://www.docker.com/products/docker-desktop/) e certifique-se de que o Docker esteja rodando.
+
+No terminal, navegue até a raiz do projeto e execute os seguintes passos:
+
+Crie a imagem Docker:
+```bash
+docker build -t nome-imagem .
+```
+Rode o container com a aplicação:
+```bash
+docker run -p 5000:5000 nome-imagem
+```
+(Caso queira usar portas diferentes, basta ajustar o número da porta conforme necessário.)
+
+Abra o navegador e acesse a aplicação através do endereço http://127.0.0.1:5000.
+
+#### 4.2 Rodar a Aplicação Localmente (sem Docker)
+Caso prefira rodar a aplicação diretamente no terminal, execute o seguinte comando:
+
+Windows
+```bash
+python app.py
+```
+Linux
+```bash
+python3 app.py
+```
+
+Isso irá iniciar a aplicação localmente. Em seguida, abra o navegador e acesse a URL http://127.0.0.1:5000.
+
+### 5. Endpoints Disponíveis
+#### 5.1 Raiz da API
+GET /
+
+Este endpoint retorna informações pessoais.
+
+Resposta:
+
+```json
+{
+    "name": "Natalia Armstrong",
+    "email": "natalia.armstronggg@gmail.com",
+    "linkedin": "https://www.linkedin.com/in/nataliaarmstrong23/"
+}
+```
+#### 5.2 Anúncios de uma Plataforma Específica
+GET /{{plataforma}}
+
+Este endpoint retorna uma tabela com os anúncios de uma plataforma específica, incluindo todos os campos de insights relacionados ao anúncio, bem como o nome da conta que está veiculando o anúncio.
+
+Exemplo de resposta:
+
+```csv
+Platform,Ad Name,Clicks,Impressions,Spend,...
+Facebook,Some Ad,10,1000,200,...
+Facebook,Other Ad,20,1500,300,...
+YouTube,One More Ad,5,500,100,...
+```
+#### 5.3 Resumo de Anúncios por Conta em uma Plataforma
+GET /{{plataforma}}/resumo
+
+Este endpoint retorna uma tabela similar à do endpoint anterior, mas colapsando os dados por conta. As colunas numéricas são somadas, e as colunas de texto podem ficam vazias (exceto para o nome da conta).
+
+Exemplo de resposta:
+
+```csv
+Platform,Ad Name,Clicks,Impressions,Spend,...
+Facebook,,30,2500,500,...
+YouTube,,5,500,100,...
+```
+
+#### 5.4 Todos os Anúncios de Todas as Plataformas
+GET /geral
+
+Este endpoint retorna todos os anúncios de todas as plataformas, incluindo colunas para identificar o nome da plataforma e o nome da conta que veicula o anúncio. Para as plataformas que não possuem um determinado campo, o valor ficará vazio.
+
+Exemplo de resposta:
+
+```csv
+Platform,Ad Name,Clicks,Impressions,Spend,...
+Facebook,Some Ad,10,1000,200,...
+Facebook,Other Ad,20,1500,300,...
+YouTube,One More Ad,5,500,100,...
+```
+
+#### 5.5 :bar_chart: Resumo de Todos os Anúncios
+GET /geral/resumo
+
+Este endpoint retorna uma tabela semelhante ao endpoint anterior, mas com a soma dos valores numéricos para cada plataforma. As colunas de texto ficam vazias (exceto para o nome da plataforma).
+
+Exemplo de resposta:
+
+```csv
+Copiar
+Platform,Ad Name,Clicks,Impressions,Spend,...
+Facebook,,30,2500,500,...
+YouTube,,5,500,100,...
+```
+### Conclusão
+Agora você pode acessar os relatórios dinâmicos das plataformas de anúncios, seja localmente ou por meio de um container Docker. Utilize os endpoints da API para visualizar os dados e gerar os relatórios necessários.
+
+Se houver dúvidas ou problemas, fique à vontade para abrir uma issue no repositório ou entrar em contato diretamente pelo email: natalia.armstronggg@gmail.com
--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, jsonify, Response
 import requests
 import pandas as pd
+import json
+from datetime import datetime
 
 app = Flask(__name__)
 
-# Dados pessoais no endpoint raiz
+def load_config():
+    with open('config.json', 'r') as config_file:
+        return json.load(config_file)
+    
+config = load_config()
+API_URL = config["API_URL"]
+TOKEN = config["TOKEN"]
+
 @app.route("/")
 def home():
     return jsonify({
@@ -13,43 +22,32 @@ def home():
         "linkedin": "https://www.linkedin.com/in/nataliaarmstrong23/"
     })
 
-# URL da API
-API_URL = "https://sidebar.stract.to/api"
-TOKEN = "ProcessoSeletivoStract2025"  # Token geral, que não é usado para as contas
-
-# Funções para consumir os endpoints da API
 def get_platforms():
-    """ Obtém a lista de plataformas disponíveis. """
     url = f"{API_URL}/platforms"
     headers = {"Authorization": TOKEN}
     response = requests.get(url, headers=headers)
     return response.json()['platforms']
 
 def get_accounts(platform):
-    """ Obtém as contas para uma plataforma específica. """
     url = f"{API_URL}/accounts?platform={platform}"
     headers = {"Authorization": TOKEN}
     response = requests.get(url, headers=headers)
     return response.json()['accounts']
 
 def get_fields(platform):
-    """ Obtém os campos disponíveis para uma plataforma. """
     url = f"{API_URL}/fields?platform={platform}"
     headers = {"Authorization": TOKEN}
     response = requests.get(url, headers=headers)
     return response.json()['fields']
 
 def get_insights(platform, account, fields):
-    """ Obtém os insights de anúncios de uma conta em uma plataforma usando o token da conta. """
     url = f"{API_URL}/insights?platform={platform}&account={account['id']}&token={account['token']}&fields={','.join(fields)}"
     headers = {"Authorization": TOKEN}
     response = requests.get(url, headers=headers)
     return response.json()['insights']
 
-# Endpoint para retornar todos os anúncios de todas as plataformas
 @app.route("/geral")
 def get_all_ads():
-    """ Retorna um CSV com todos os anúncios de todas as plataformas. """
     platforms = get_platforms()
     all_ads = []
 
@@ -57,7 +55,6 @@ def get_all_ads():
         accounts = get_accounts(platform['value'])
         fields = get_fields(platform['value'])
         
-        # Mapeando os campos (text como header e value como chave dos dados)
         field_map = {field['value']: field['text'] for field in fields}
         
         for account in accounts:
@@ -67,21 +64,20 @@ def get_all_ads():
                 ad["Account"] = account['name']
                 all_ads.append(ad)
 
-    # Criação do DataFrame com as colunas dinâmicas baseadas nos "text"
     df = pd.DataFrame(all_ads)
     df = df.rename(columns=field_map)
 
-    # Somando os valores numéricos para cada plataforma e conta
     numeric_columns = df.select_dtypes(include=['number']).columns
     df_summed = df.groupby(['Platform', 'Account'])[numeric_columns].sum().reset_index()
 
-    # Gerar o CSV com todos os dados agregados
+
+    current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    filename = f"{current_time}_geral.csv"
     csv_data = df_summed.to_csv(index=False)
-    return Response(csv_data, mimetype="text/csv", headers={"Content-Disposition": "attachment; filename=geral.csv"})
+    return Response(csv_data, mimetype="text/csv", headers={"Content-Disposition": f"attachment; filename={filename}"})
 
 @app.route("/geral/resumo")
 def get_summary_ads():
-    """ Retorna um resumo CSV com todos os anúncios de todas as plataformas. """
     platforms = get_platforms()
     summary_ads = []
 
@@ -89,10 +85,9 @@ def get_summary_ads():
         accounts = get_accounts(platform['value'])
         fields = get_fields(platform['value'])
 
-        # Mapeando os campos (text como header e value como chave dos dados)
         field_map = {field['value']: field['text'] for field in fields}
         
-        all_platform_data = []  # Armazenar todos os anúncios dessa plataforma
+        all_platform_data = [] 
 
         for account in accounts:
             insights = get_insights(platform['value'], account, list(field_map.keys()))
@@ -104,24 +99,20 @@ def get_summary_ads():
         df = pd.DataFrame(all_platform_data)
         df = df.rename(columns=field_map)
 
-        # Somando os valores numéricos para cada plataforma
         numeric_columns = df.select_dtypes(include=['number']).columns
         df_summed = df.groupby(['Platform'])[numeric_columns].sum().reset_index()
 
-        # Gerar o CSV com os dados agregados
         summary_ads.extend(df_summed.to_dict(orient='records'))
 
-    # Criar o DataFrame para o resumo
     df_summary = pd.DataFrame(summary_ads)
 
-    # Gerar o CSV com os dados agregados
+    current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    filename = f"{current_time}_geral_resumo.csv"
     csv_data = df_summary.to_csv(index=False)
-    return Response(csv_data, mimetype="text/csv", headers={"Content-Disposition": "attachment; filename=geral_resumo.csv"})
+    return Response(csv_data, mimetype="text/csv", headers={"Content-Disposition": f"attachment; filename={filename}"})
 
-# Endpoint para retornar os anúncios de uma plataforma específica
 @app.route("/<platform>")
 def get_platform_ads(platform):
-    """ Retorna um CSV com todos os anúncios de uma plataforma específica. """
     platforms = get_platforms()
     platform_data = next((plat for plat in platforms if plat['value'] == platform), None)
 
@@ -140,10 +131,11 @@ def get_platform_ads(platform):
             all_ads.append(ad)
 
     df = pd.DataFrame(all_ads)
+    current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    filename = f"{current_time}_{platform}.csv"
     csv_data = df.to_csv(index=False)
-    return Response(csv_data, mimetype="text/csv", headers={"Content-Disposition": f"attachment; filename={platform}_ads.csv"})
+    return Response(csv_data, mimetype="text/csv", headers={"Content-Disposition": f"attachment; filename={filename}"})
 
-# Endpoint para retornar o resumo de uma plataforma específica
 @app.route("/<platform>/resumo")
 def get_platform_summary(platform):
     """ Retorna um resumo CSV de uma plataforma específica. """
@@ -157,10 +149,9 @@ def get_platform_summary(platform):
     fields = get_fields(platform_data['value'])
     summary_ads = []
 
-    # Mapeando os campos (text como header e value como chave dos dados)
     field_map = {field['value']: field['text'] for field in fields}
 
-    all_platform_data = []  # Armazenar todos os anúncios dessa plataforma
+    all_platform_data = [] 
 
     for account in accounts:
         insights = get_insights(platform_data['value'], account, list(field_map.keys()))
@@ -172,19 +163,18 @@ def get_platform_summary(platform):
     df = pd.DataFrame(all_platform_data)
     df = df.rename(columns=field_map)
 
-    # Somando os valores numéricos para cada conta e plataforma
     numeric_columns = df.select_dtypes(include=['number']).columns
     df_summed = df.groupby(['Platform', 'Account'])[numeric_columns].sum().reset_index()
 
-    # Gerar o CSV com os dados agregados
     summary_ads.extend(df_summed.to_dict(orient='records'))
 
-    # Criar o DataFrame para o resumo
+
     df_summary = pd.DataFrame(summary_ads)
 
-    # Gerar o CSV com os dados agregados
+    current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    filename = f"{current_time}_{platform}_resumo.csv"
     csv_data = df_summary.to_csv(index=False)
-    return Response(csv_data, mimetype="text/csv", headers={"Content-Disposition": f"attachment; filename={platform}_resumo.csv"})
+    return Response(csv_data, mimetype="text/csv", headers={"Content-Disposition": f"attachment; filename={filename}"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
